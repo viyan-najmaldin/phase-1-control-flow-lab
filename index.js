3a200f8b02ea54e9ff1b3cf60805f65e3c68c686
viyan-najmaldin <najmadinviyan@gmail.com>
2022-09-13 19:16:39 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a) {
   // Write your code here!
+  if (a <= 400) {
+    return "This one is on me!";
+  } else if (a < 2000 && a > 400) {
+    return "That will be twenty bucks.";
+  } else if (a > 2500) {
+    return "No can do.";
+  } else if (a > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
-
-function ternaryCheckCity(){
+console.log(scuberGreetingForFeet(2501));
+function ternaryCheckCity(b) {
   // Write your code here!
+  return b === "NYC" ? "Ok, sounds good." : "No go.";
 }
-
-function switchOnCharmFromTip(){
+console.log(ternaryCheckCity("NYC"));
+function switchOnCharmFromTip(t) {
   // Write your code here!
+  switch (t) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    case "thanks for everything":
+      return "Bye.";
+  }
 }
+console.log(switchOnCharmFromTip("generous"));
